1652dcfb8c9f4b58637754e96d723074fc1c10f4
LindelaniMKH <dlaminlindelani.15@gmail.com>
2025-09-23 12:59:19 +0200

    Added background for Login Page form

--- frontend/src/assets/Components/LoginPage.jsx
+++ frontend/src/assets/Components/LoginPage.jsx
@@ -3,33 +3,34 @@
 export default function LoginPage(){
     return(
     <>
-        <div className="flex justify-center w-screen">
+        <div className="bg-gradient-to-br from-violet-400 to-orange-400 flex justify-center w-screen h-screen pt-[15vh]">
             <div className="bg-[#F0F0F0] rounded-3xl w-[350px] h-[475px] flex flex-col justify-center p-4">
 
                 <h2 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-orange-400 to-violet-400 text-2xl"><strong>Collaboration Hub</strong></h2>
 
                 <p className="text-[#000000] text-center">Welcome back! Sign in to<br/>access your collaboration<br/>workspace.</p>
 
+                {/*Email section*/}
                 <div className="py-2">
                     <p className="text-[#000000]">Email Address</p>
-                    <input className="outline-solid outline-violet-400 outline-2 rounded-sm text-[#000000] w-full h-8" placeholder="Enter your Email" autoFocus></input>
+                    <input className="outline-solid outline-violet-400 outline-2 rounded-sm text-[#000000] w-full h-8" placeholder=" Enter your Email" autoFocus></input>
                 </div>
 
-
+                {/*Password section*/}
                 <div className="pt-2 pb-1.5">
                     <p className="text-[#000000]">Password</p>
-                    <input className="outline-solid outline-violet-400 outline-2 rounded-sm text-[#000000] w-full h-8" placeholder="Enter your Password"></input>
+                    <input className="outline-solid outline-violet-400 outline-2 rounded-sm text-[#000000] w-full h-8" placeholder=" Enter your Password"></input>
                 </div>
 
                 <div className="flex flex-row justify-between pb-8">
                     <div className="flex flex-row">
                         <input className="" type="checkbox"></input>
-                        <p className="text-[#000000]">Remember Me</p>
+                        <p className="text-[#000000] pl-2 inline">Remember Me</p>
                     </div>
-                    <p className="text-violet-400">Forgot Password?</p>
+                    <p className="text-violet-400"><u>Forgot Password?</u></p>
                 </div>
 
-                <button className="bg-gradient-to-r from-orange-300 to-violet-400 text-[#000000]">Sign In</button>
+                <button className="border-none bg-gradient-to-r from-orange-300 to-violet-400 text-[#000000]">Sign In</button>
 
                 <div className="flex flex-row text-xs justify-center py-4">
                     <p className="text-[#000000]">Don't have an account?</p>
