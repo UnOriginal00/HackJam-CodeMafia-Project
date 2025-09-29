8e7725f0953a822e8150447cc6b2def3904a411c
Ripfumelo Junior Chauke <pfumichauke4@gmail.com>
2025-09-29 21:21:42 +0200

    Configured backend for more reliable API connections

--- backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca53567160575006eff9497b638adf9e76a82e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02512aef6ac5efbe11002f052f6bfb7b82e427a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
