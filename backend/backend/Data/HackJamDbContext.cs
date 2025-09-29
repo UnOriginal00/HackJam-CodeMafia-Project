22934becd17456b67564c252cebbcd67e37fd399
Ripfumelo Junior Chauke <pfumichauke4@gmail.com>
2025-09-29 23:29:40 +0200

    Improved Scafolding with as well as set up a connetion to the database

---
+++ backend/backend/Data/HackJamDbContext.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace backend.Data
+{
+    public class HackJamDbContext : DbContext
+    {  
+
+        public HackJamDbContext(DbContextOptions<HackJamDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<User_Details> UserDetails { get; set; }
+    }
+}
